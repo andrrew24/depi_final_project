a2db27f9353843f92bf0f7f4c40bab2a4037b5b2
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-10-07 13:16:06 +0300

    just for testing

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,22 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movie_app/config/theme/app_color.dart';
 import 'package:movie_app/config/theme/theme.dart';
 import 'package:movie_app/features/details/views/details_view.dart';
+import 'package:movie_app/service/api_service.dart';
 
 void main() async {
   // load variables in .env file (placed in the root project folder)
   await dotenv.load(fileName: ".env");
 
   runApp(const MoviesApp());
+
+  final network = NetworkService();
+  try {
+    final res = await network.getMovieById(672);
+    print(res);
+    print(res.toJson());
+  } catch (error) {
+    print("error is $error");
+  }
 
   SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(statusBarColor: AppColor.appBackgroundColor));
