1bc0c736ed09c4db252292695127eb516c6fee72
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-10-07 11:37:10 +0300

    imported api key from .env file

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,1 +1,38 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:movie_app/service/DTOs/MovieDetailsResponseDTO.dart';
+import 'package:movie_app/service/DTOs/TrendingMoviesResponseDTO.dart';
+
+class NetworkService {
+  final Dio _dio = Dio();
+  final String _baseUrl = 'https://api.themoviedb.org/3';
+  final String _apiKey = dotenv.env['TMDB_API_KEY'] ?? ''; // Replace with your actual TMDB API key
+
+  NetworkService() {
+    _dio.options.baseUrl = _baseUrl;
+    _dio.options.queryParameters = {'api_key': _apiKey};
+  }
+
+  /// to get Movie By its id when user clicks on any movie item
+  Future<MovieDetailsResponseDto> getMovieById(int id) async {
+    try {
+      final response = await _dio.get('/movie/$id');
+      return response.data;
+    } catch (error) {
+      print('Error fetching movie by ID: $error');
+      rethrow;
+    }
+  }
+
+  /// to get trending movies that appears in the top of home screen
+  Future<TrendingMoviesResponseDto> getTrendingMovies() async {
+    try {
+      final response = await _dio.get('/trending/movie/week');
+      return response.data;
+    } catch (error) {
+      print('Error fetching trending movies: $error');
+      rethrow;
+    }
+  }
+}
 // Dio Api Service
