510df6b9d9791b9879dd015a9bae09a6b840e3b6
andrrew24 <91123740+andrrew24@users.noreply.github.com>
2024-10-08 11:39:37 +0300

    added getMoviesByCategory to api service

---
+++ lib/config/constants/app_constants.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  AppConstants._();
+  
+  // Constants of Movie Categoires
+  static const kNowPlayingCategory = "now_playing";
+  static const kTopRatedCategory = "top_rated";
+  static const kUpcomingCategory = "upcoming";
+  static const kPopularCategory = "popular";
+}
