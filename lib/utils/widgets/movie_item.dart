f98c08043e10d345382b5b725bbaebdb8fbef323
andrrew24 <91123740+andrrew24@users.noreply.github.com>
2024-10-08 15:57:55 +0300

    edited movie item

--- lib/utils/widgets/movie_item.dart
+++ lib/utils/widgets/movie_item.dart
@@ -1,13 +1,19 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 import 'package:movie_app/config/constants/app_assets.dart';
+import 'package:movie_app/config/constants/app_constants.dart';
 import 'package:movie_app/config/theme/app_styles.dart';
+import 'package:movie_app/features/home/models/movies_model.dart';
 
 class MovieItem extends StatelessWidget {
   const MovieItem({
     super.key,
+    required this.moviesModel,
   });
+
+  final MoviesModel moviesModel;
 
   @override
   Widget build(BuildContext context) {
@@ -19,19 +25,30 @@ class MovieItem extends StatelessWidget {
             height: 150,
             width: 100,
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: const DecorationImage(
-                    fit: BoxFit.fitHeight,
-                    image: AssetImage(AppAssets.imgDefaultmovieimg))),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: CachedNetworkImage(
+                imageUrl: moviesModel.backdropPath == null
+                    ? AppConstants.kNoImgPreviewLink
+                    : "${AppConstants.kStaticImgLink}${moviesModel.backdropPath}",
+                fit: BoxFit.fitHeight,
+              ),
+            ),
           ),
           const Gap(20),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                "Spider Man",
-                style: AppStyles.stylePoppinsRegular16(),
+              SizedBox(
+                width: 200,
+                child: Text(
+                  overflow: TextOverflow.ellipsis,
+                  moviesModel.title ?? "No name",
+                  style: AppStyles.stylePoppinsRegular16(),
+                ),
               ),
               const Gap(14),
               Row(
@@ -42,7 +59,9 @@ class MovieItem extends StatelessWidget {
                   ),
                   const Gap(5),
                   Text(
-                    "8.5",
+                    moviesModel.voteAverage == null
+                        ? "No Rating"
+                        : moviesModel.voteAverage!.toStringAsFixed(1),
                     style: AppStyles.styleMontserratSemiBold12(),
                   )
                 ],
@@ -70,7 +89,7 @@ class MovieItem extends StatelessWidget {
                   ),
                   const Gap(5),
                   Text(
-                    "2019",
+                    moviesModel.releaseDate ?? "NO Date",
                     style: AppStyles.stylePoppinsRegular12(),
                   )
                 ],
