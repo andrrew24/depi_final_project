c6817a545cb4c9e5e2b1d0a5bdb903d205c2ad27
andrrew24 <91123740+andrrew24@users.noreply.github.com>
2024-10-14 01:06:00 +0300

    wraped movie item with gesture detector

--- lib/utils/widgets/movie_item.dart
+++ lib/utils/widgets/movie_item.dart
@@ -11,105 +11,99 @@ class MovieItem extends StatelessWidget {
   const MovieItem({
     super.key,
     required this.moviesModel,
+    required this.onTap,
   });
 
   final MoviesModel moviesModel;
+  final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 150,
-      child: Row(
-        children: [
-          Container(
-            height: 150,
-            width: 100,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: CachedNetworkImage(
-                imageUrl:
-                    "${AppConstants.kStaticImgLink}${moviesModel.posterPath}",
-                fit: BoxFit.fitHeight,
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        height: 150,
+        child: Row(
+          children: [
+            Container(
+              height: 150,
+              width: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
               ),
-            ),
-          ),
-          const Gap(20),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 200,
-                child: Text(
-                  overflow: TextOverflow.ellipsis,
-                  moviesModel.title ?? "No name",
-                  style: AppStyles.stylePoppinsRegular16(),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: CachedNetworkImage(
+                  imageUrl:
+                      "${AppConstants.kStaticImgLink}${moviesModel.posterPath}",
+                  fit: BoxFit.fitHeight,
                 ),
               ),
-              const Gap(14),
-              Row(
-                children: [
-                  SvgPicture.asset(
-                    AppAssets.imgStar,
-                    width: 20,
+            ),
+            const Gap(20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 200),
+                  child: Text(
+                    moviesModel.title ?? "No name",
+                    style: AppStyles.stylePoppinsRegular16(),
                   ),
-                  const Gap(5),
-                  Text(
-                    moviesModel.voteAverage == null
-                        ? "No Rating"
-                        : moviesModel.voteAverage!.toStringAsFixed(1),
-                    style: AppStyles.styleMontserratSemiBold12(),
-                  )
-                ],
-              ),
-              const Gap(4),
-              Row(
-                children: [
-                  SvgPicture.asset(
-                    AppAssets.imgTicket,
-                    width: 20,
+                ),
+                const Gap(14),
+                Row(
+                  children: [
+                    SvgPicture.asset(
+                      AppAssets.imgStar,
+                      width: 20,
+                    ),
+                    const Gap(5),
+                    Text(
+                      moviesModel.voteAverage == null
+                          ? "No Rating"
+                          : moviesModel.voteAverage!.toStringAsFixed(1),
+                      style: AppStyles.styleMontserratSemiBold12(),
+                    )
+                  ],
+                ),
+                const Gap(4),
+                Row(
+                  children: [
+                    SvgPicture.asset(
+                      AppAssets.imgTicket,
+                      width: 20,
+                    ),
+                    const Gap(5),
+                    Text(
+                      moviesModel.genre ?? " ",
+                      style: AppStyles.stylePoppinsRegular12(),
+                    )
+                  ],
+                ),
+                const Gap(4),
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: Row(
+                    children: [
+                      SvgPicture.asset(
+                        AppAssets.imgCalendarBlank,
+                        width: 20,
+                      ),
+                      const Gap(5),
+                      Text(
+                        moviesModel.releaseDate ?? "NO Date",
+                        style: AppStyles.stylePoppinsRegular12(),
+                      )
+                    ],
                   ),
-                  const Gap(5),
-                  Text(
-                    "Action",
-                    style: AppStyles.stylePoppinsRegular12(),
-                  )
-                ],
-              ),
-              const Gap(4),
-              Row(
-                children: [
-                  SvgPicture.asset(
-                    AppAssets.imgCalendarBlank,
-                    width: 20,
-                  ),
-                  const Gap(5),
-                  Text(
-                    moviesModel.releaseDate ?? "NO Date",
-                    style: AppStyles.stylePoppinsRegular12(),
-                  )
-                ],
-              ),
-              const Gap(4),
-              Row(
-                children: [
-                  SvgPicture.asset(
-                    AppAssets.imgClock,
-                    width: 20,
-                  ),
-                  const Gap(5),
-                  Text(
-                    "139",
-                    style: AppStyles.stylePoppinsRegular12(),
-                  )
-                ],
-              ),
-            ],
-          )
-        ],
+                ),
+                const Gap(4),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
