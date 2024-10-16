bbae8c400789957521669b5919c0ae79796c5d59
andrrew24 <91123740+andrrew24@users.noreply.github.com>
2024-10-16 18:25:24 +0300

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movie_app/config/service_locator/service_locator.dart';
@@ -5,13 +6,15 @@ import 'package:movie_app/config/theme/theme.dart';
 import 'package:movie_app/features/home/views/home.dart';
 import 'package:movie_app/features/splash_screen/views/splash_screen.dart';
 
-
 void main() async {
   // load variables in .env file (placed in the root project folder)
   await dotenv.load(fileName: ".env");
-  
+
+  await Firebase.initializeApp();
+
   //setup service locator for dependency inject
-   setupServiceLocator();
+  setupServiceLocator();
+
   runApp(const MoviesApp());
 }
 
@@ -21,14 +24,12 @@ class MoviesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.darkTheme,
-      initialRoute: "/splash",
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.darkTheme,
+        initialRoute: "/splash",
         routes: {
           '/splash': (context) => const SplashScreen(durationInSeconds: 5),
           '/home': (context) => const Home(),
-          
-        }
-    );
+        });
   }
 }
